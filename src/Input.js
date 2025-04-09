9e256c31aaaefb9ea6e0741600b4919b32a1794b
EnriqueEnriquez <ebenriquez@up.edu.ph>
2025-04-09 16:12:58 +0800

    Refactored input handling

--- src/Input.js
+++ src/Input.js
@@ -3,7 +3,6 @@ import { useState } from "react"
 function InputSection() {
     const[minterms, setMinterms] = useState(""); 
     const[variables, setVariables] = useState(""); 
-    const[errorText, setErrorText] = useState(""); 
 
     return (
         <div className="input-container">
@@ -11,8 +10,7 @@ function InputSection() {
             <div className="sub-container">
                 <MintermsInput value={minterms} retrieveMinterms={setMinterms}/>
                 <VariablesInput value={variables} retrieveVariables={setVariables}/>
-                <p></p>
-                <SolveButton onButtonClick={() => HandleInputs(minterms, variables)}/>
+                <SolveButton HandleInputs={() => HandleInputs(minterms, variables)}/>
             </div>
 
         </div>
@@ -49,11 +47,36 @@ function VariablesInput({value, retrieveVariables}) {
     )
 }
 
-function SolveButton({onButtonClick}) {
+function SolveButton({HandleInputs}) {
+
+    /* 
+    Have an array of text
+    If the array is not empty, add a try again at the end of the error statement and invalid input at the start of the error
+    Connect them
+    [mintermsFormatCorrect, noMintermDuplicates, noNonDigit, variablesFormatCorrect, noNonLetter, noVariableDuplicates, sufficientVariables]
+    */
+
+    function onButtonClick() {
+        let handleInputsArray = HandleInputs(); 
+        let errorText = ''; 
+        for(let index = 0; index < handleInputsArray.length; index++) {
+            if (handleInputsArray[index] !== '') {
+                errorText = errorText + handleInputsArray[index] + '. '; 
+            }
+        }
+
+        if (errorText === '') {
+            console.log("Proceed to next step"); 
+        } 
+        else {
+            errorText = errorText + "Please try again."
+        }
+
+    }
 
     return(
         <div>
-
+            <p>{errorText}</p>
             <button className="solve-button" onClick={onButtonClick}>Solve</button>
         </div>
         
@@ -61,93 +84,90 @@ function SolveButton({onButtonClick}) {
 }
 
 function HandleInputs(minterms, variables) {
-/* 
-minterms
-1. remove any leading or trailing white spaces
-2. split via , and store them in an array
-3. Check for any duplicates
-4. Check if it is a digit
-5. Check if correct format of , 
-*/
+    /* 
+    minterms
+    1. remove any leading or trailing white spaces
+    2. split via , and store them in an array
+    3. Check for any duplicates
+    4. Check if it is a digit
+    5. Check if correct format of , 
+    */
 
-let mintermsFormatCorrect = true;
-let noMintermDuplicates = true; 
-let noNonDigit = true;
-let maxNumber; 
+    let mintermsFormatMessage = '';
+    let mintermDuplicatesMesage = ''; 
+    let nonDigitMessage = '';
+    let maxNumber; 
 
-minterms = minterms.trim(); 
-let mintermsArray = minterms.split(','); 
+    minterms = minterms.trim(); 
+    let mintermsArray = minterms.split(','); 
 
-for (let index = 0; index < mintermsArray.length; index++) {
-    if(!Number.isInteger(parseFloat(mintermsArray[index]))) {
-        noNonDigit = false; 
-    }
-    else {
-        mintermsArray[index] = parseFloat(mintermsArray[index]); 
-    }
-}
-
-const mintermsDuplicates = mintermsArray.filter((item, index) => mintermsArray.indexOf(item) !== index)
-
-if (mintermsDuplicates.length != 0) {
-    noMintermDuplicates = false; 
-}
-
-
-if (noNonDigit === true) {
-    mintermsArray.sort(); 
-    maxNumber = mintermsArray.at(-1); 
-}
-
-console.log("Minterms"); 
-console.log(noMintermDuplicates); 
-console.log(noNonDigit); 
-/* 
-variables 
-1. remove any leading or trailing white spaces
-2. transform into an array
-3. Check for any duplicates. 
-4. Check if it has sufficient variables depending on the minterms. 
-*/
-
-variables = variables.trim(); 
-let variablesArray = []; 
-let variablesFormatCorrect = true; 
-let noNonLetter = true; 
-let noVariableDuplicates = true; 
-let sufficientVariables = true;
-
-for (let index = 0; index < variables.length; index++) {
-    let char = variables.charAt(index); 
-
-    if (char === " ") {
-        variablesFormatCorrect = false;
-    } else {
-        variablesArray.push(char);
-        if (!/[a-zA-Z]/.test(char)) {
-            noNonLetter = false;
+    for (let index = 0; index < mintermsArray.length; index++) {
+        let element = parseFloat(mintermsArray[index])
+        if(!Number.isInteger(element) || element < 0) {
+            nonDigitMessage = 'Minterms should only be positive integers'; 
+        } 
+        else {
+            mintermsArray[index] = parseFloat(mintermsArray[index]); 
         }
     }
-}
 
+    const mintermsDuplicates = mintermsArray.filter((item, index) => mintermsArray.indexOf(item) !== index)
 
-const variablesDuplicates = variablesArray.filter((item, index) => variablesArray.indexOf(item) !== index)
-
-if (variablesDuplicates.length != 0) {
-    noVariableDuplicates = false; 
-}
-
-if (noVariableDuplicates == true) {
-    if (Math.pow(2, variablesArray.length) - 1 < maxNumber) {
-        sufficientVariables = false; 
+    if (mintermsDuplicates.length !== 0) {
+        mintermDuplicatesMesage = 'Minterms should have no duplicates'; 
     }
-}
 
-console.log("Variables")
-console.log(variablesFormatCorrect);
-console.log(noNonLetter); 
-console.log(noVariableDuplicates); 
-console.log(sufficientVariables); 
+    if (noNonDigit === true) {
+        mintermsArray.sort(); 
+        maxNumber = mintermsArray.at(-1); 
+    }
+
+    /* 
+    variables 
+    1. remove any leading or trailing white spaces
+    2. transform into an array
+    3. Check for any duplicates. 
+    4. Check if it has sufficient variables depending on the minterms. 
+    */
+
+    variables = variables.trim(); 
+    let variablesArray = []; 
+    let variablesFormatMessage = ''; 
+    let nonLetterMessage = '';
+    let variableDuplicatesMessage = ''; 
+    let sufficientVariablesMessage = '';
+
+    for (let index = 0; index < variables.length; index++) {
+        let char = variables.charAt(index); 
+
+        if (char === " ") {
+            variablesFormatMessage = 'Incorrect variables format. Ensure there are no spaces';
+        } else {
+            variablesArray.push(char);
+            if (!/[a-zA-Z]/.test(char)) {
+                nonLetterMessage = 'Variables should only be letters of the English Alphabet';
+            }
+        }
+    }
+
+
+    const variablesDuplicates = variablesArray.filter((item, index) => variablesArray.indexOf(item) !== index)
+
+    if (variablesDuplicates.length !== 0) {
+        variableDuplicatesMessage = 'Variables should have no duplicates'; 
+    }
+
+    if (noVariableDuplicates === true) {
+        if (Math.pow(2, variablesArray.length) - 1 < maxNumber) {
+            let quantityVariables = variablesArray.length; 
+            while(Math.pow(2, quantityVariables) - 1 < maxNumber) {
+                quantityVariables++; 
+            }
+            sufficientVariablesMessage = 'Number of variables should be at least ' + quantityVariables; 
+        }
+    }
+
+    return [mintermsFormatMessage, mintermDuplicatesMesage, nonDigitMessage, variablesFormatMessage, nonLetterMessage, variableDuplicatesMessage, sufficientVariablesMessage]; 
 
 }
 
